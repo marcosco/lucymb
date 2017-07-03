9ed860d1e40a8d21920ef46c99d94cc023d26c8f
Alessio Rocchi <rocchi.b.a@gmail.com>
2017-07-03 22:40:29 +0200

    Refactored master.py, making it compatible with Python 3, adding argparse to parse parameters from command line.

--- master.py
+++ master.py
@@ -1,27 +1,48 @@
 #!/usr/bin/env python
-import serial
-import time
+from __future__ import print_function
+
+try:
+    import __builtin__
+
+    getattr(__builtin__, 'raw_input')
+    # Python 2.*
+    input_function = raw_input
+except (ImportError, AttributeError):
+    # Python 3.*
+    input_function = input
+    pass
+
 import sys
 
+import serial
+
 from lucy.lucyhub import LucyHub
-from lucy.exceptions import DeviceNotFound, InvalidOperation
+
+from argparse import ArgumentParser
 
 PORT = "/dev/ttyUSB0"
-#BAUDRATE = 57600
 BAUDRATE = 9600
-def main():
-	rtu_connection = serial.Serial(port=PORT, baudrate=BAUDRATE, bytesize=8, parity='N', stopbits=1, xonxoff=0)
-	lucy = LucyHub(rtu_connection)
-	print "Lucy is online!"
 
-	lucy.start()
-	print("Waiting for user input")
-	while True:
-		msg = raw_input("Command To Send: ")
-		if msg == "close":
-			lucy.stop()
-			lucy.dump()
-			sys.exit(0)
+
+def main():
+    p = ArgumentParser(prog='Lucy')
+    p.add_argument('-p', '--port', help='Serial Port', type=str, action='store', required=True, dest='port')
+    p.add_argument('-b', '--baudrate', help='Baudrate for Serial Port', type='int', action='store', required=True,
+                   dest='baudrate')
+    p.parse_args()
+
+    rtu_connection = serial.Serial(port=p.port, baudrate=p.baudrate, bytesize=8, parity='N', stopbits=1, xonxoff=0)
+    lucy = LucyHub(rtu_connection)
+    print("Lucy is online!")
+
+    lucy.start()
+    print("Waiting for user input")
+    while True:
+        msg = input_function("Command To Send: ")
+        if msg == "close":
+            lucy.stop()
+            lucy.dump()
+            sys.exit(0)
 
 
 if __name__ == "__main__":

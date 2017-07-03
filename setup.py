8b0720b4ad11b6ea7cbea1c9b3987317a3e0894b
Alessio Rocchi <rocchi.b.a@gmail.com>
2017-07-03 22:30:58 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Lucy',
+    version='0.0.1',
+    packages=['lucy', 'lucy.test'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
